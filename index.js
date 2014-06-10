7d10800717cdb08f4e82f0c4e166fe090c9d9baa
calebmfreed <calebmfreed@gmail.com>
2014-06-10 17:30:32 -0500

    trying nrf again

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ radio.begin(function () {
     console.log(tx);
     console.log(rx);
     rx.pipe(tx);        // echo back everything
+    radio.sendPayload(Buffer("HUH"), ,function(){console.log("SHIt?")});
 });
 
 
