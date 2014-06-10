18375c7e65971951dd50cf32d4e8820049bbba3c
calebmfreed <calebmfreed@gmail.com>
2014-06-10 16:34:32 -0500

    trying nrf again

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var http = require('http'),
 var radio = require('nrf').connect(spiDev, cePin, irqPin);
 radio.channel(0x4c).dataRate('1Mbps').crcBytes(2).autoRetransmit({count:15, delay:4000});
 radio.begin(function () {
-    var rx = radio.openPipe('rx', 0xF0F0F0F0E1LL),
-        tx = radio.openPipe('tx', 0xF0F0F0F0D2LL);
+    var rx = radio.openPipe('rx', 0xF0F0F0F0E1),
+        tx = radio.openPipe('tx', 0xF0F0F0F0D2);
     rx.pipe(tx);        // echo back everything
 });
 
