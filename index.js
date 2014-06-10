52a1537ecbf4d69b80fd7e3234b91061b7d4ac4c
calebmfreed <calebmfreed@gmail.com>
2014-06-10 02:34:51 -0500

    adding test for spi on pi

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ var room2 = 0;
 var http = require('http'),
     express = require('express'),
     path = require('path');
+
+var SPI = require('pi-spi');
+
+var spi = SPI.initialize("/dev/spidev0.0"),
+    test = Buffer("Hello, World!");
+
+// reads and writes simultaneously
+spi.transfer(test, test.length, function (e,d) {
+    if (e) console.error(e);
+    else console.log("Got \""+d.toString()+"\" back.");
+
+    if (test.toString() === d.toString()) {
+        console.log(msg);
+    } else {
+        // NOTE: this will likely happen unless MISO is jumpered to MOSI
+        console.warn(msg);
+        process.exit(-2);
+    }
+});
  
 var app = express();
 app.set('port', process.env.PORT || 3000);
