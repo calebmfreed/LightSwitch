d688115a9eb3e7cc73569037b3edd3183b02e2b2
calebmfreed <calebmfreed@gmail.com>
2014-06-10 19:03:24 -0500

    trying nrf again

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ radio.begin(function () {
     var rx = radio.openPipe('rx', 0xF0F0F0F0E1),
         tx = radio.openPipe('tx', 0xF0F0F0F0D2);
     console.log(tx);
-    console.log(rx);
+    //console.log(rx);
     rx.pipe(tx);        // echo back everything
 });
 
